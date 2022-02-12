e9b2546b10be366a24231144bd2e2fd0ad445b3d
barPerlman <bar.perlman9@gmail.com>
2022-02-12 12:22:03 +0200

    home page ordered

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BsNavbarComponent
   ],
   imports: [
     BrowserModule,

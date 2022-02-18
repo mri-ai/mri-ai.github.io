bbb8f7c9422c6185836f48c04e68f54b3e09b1fa
barPerlman <bar.perlman9@gmail.com>
2022-02-18 15:00:23 +0200

    home page with navbar fits for mobile

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+    const toggleButton = document.getElementsByClassName('toggle-button')[0];
+    const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+    toggleButton.addEventListener('click', () => {
+      navbarLinks.classList.toggle('active');
+    });
   }
 
 }

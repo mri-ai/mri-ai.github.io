a21c927eeb1cefb34ccabcaea84e9b0c68bafdcd
barPerlman <bar.perlman9@gmail.com>
2022-03-30 21:09:23 +0300

    listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bio Medical Image Lab';
+  public constructor() {
+    window.addEventListener('error', (event) => {
+      console.log('The error is:::' + event)
+    });
+  }
 
 }

ac6b76c175a425a74d71175ceca5f04680a302dd
barPerlman <bar.perlman9@gmail.com>
2022-04-26 20:34:05 +0300

    software comment

--- src/app/software/software.component.ts
+++ src/app/software/software.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SoftwareComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    //
+  }
 
   ngOnInit(): void {
   }
